d1bdf416d371566246c364f4b236efc6a437e2b6
Your Name <your_email@address>
2023-08-16 17:15:10 +0100

    updated refresh

--- src/game.js
+++ src/game.js
@@ -15,7 +15,7 @@ const gameBoard = document.getElementById("game-board");
 function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press ok to restart')) {
-      window.location = '/js_snake'
+      window.location = '/js_snake/'
     }
     return
   }
